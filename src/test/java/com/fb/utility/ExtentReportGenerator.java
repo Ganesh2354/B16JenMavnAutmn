1f6fbc49e8392144cdd6be3f6b33146ea20bb7af
Ganesh Dandekar <ganeshrajdandekar68@gmail.com>
2023-05-30 11:53:10 +0530

    7

--- src/test/java/com/fb/utility/ExtentReportGenerator.java
+++ src/test/java/com/fb/utility/ExtentReportGenerator.java
@@ -10,7 +10,7 @@ public class ExtentReportGenerator {
 	public static ExtentReports generateReport()
 	{
 
-		String path="C:\\Users\\Ganesh Dandekar\\eclipse-workspace\\B16E_Framework\\Test_Reports\\LoginFB\\JenkinsRun\\JenkinsIndex1.html";
+		String path="C:\\Users\\Ganesh Dandekar\\eclipse-workspace\\B16E_Framework\\Test_Reports\\LoginFB\\JenkinsRun\\JenkinsIndexLocalHost.html";
 		ExtentSparkReporter report=new ExtentSparkReporter(path);
 		report.config().setDocumentTitle("Facebook WebApplication Framework");
 		report.config().setReportName("LoginFBTC001");
